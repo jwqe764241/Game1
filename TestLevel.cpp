62383e41f02400bd61b61d0ee922136cd048e5fb
Gem_Ge_Jun     ; ) <jwqe764241@gmail.com>
2017-04-02 15:00:43 +0900

    TestLevel2 수정

--- TestLevel.cpp
+++ TestLevel.cpp
@@ -2,8 +2,8 @@
 
 
 
-TestLevel::TestLevel(DX_Input * pInput):
-	 m_bIsInitialized(true),
+TestLevel::TestLevel(DX_Input * pInput)
+	:m_bIsInitialized(true),
 	 m_pInput(pInput),
 	 m_SpriteSheet(SpriteSheet(L"Image/LevelTile.png"))
 {
@@ -12,11 +12,11 @@ TestLevel::TestLevel(DX_Input * pInput):
 		m_SpriteSheet.GetSize().width,
 		Graphics::GetInstance()->GetRenderTarget()->GetSize().height);
 }
-TestLevel::TestLevel(Player * player, DX_Input * pInput):
-	 m_bIsInitialized(true),
-	 m_pPlayer(player),
+TestLevel::TestLevel(DX_Input * pInput, Player * pPlayer)
+	:m_bIsInitialized(true),
+	 m_pPlayer(pPlayer),
 	 m_pInput(pInput),
-	m_SpriteSheet(SpriteSheet(L"Image/LevelTile.png"))
+	 m_SpriteSheet(SpriteSheet(L"Image/LevelTile.png"))
 {
 }
 
@@ -34,7 +34,6 @@ void TestLevel::Load()
 	m_RenderEnemy.push_back(Enemy(1000, 500));
 	m_RenderEnemy.push_back(Enemy(2000, 800));
 	m_RenderEnemy.push_back(Enemy(3500, 600));
-
 }
 
 void TestLevel::Unload() 
@@ -46,10 +45,9 @@ void TestLevel::Render()
 	RECT rect; ::GetWindowRect(Graphics::GetInstance()->GetRenderTarget()->GetHwnd(), &rect);
 
 	D2D1_SIZE_F windowSize = Graphics::GetInstance()->GetRenderTarget()->GetSize();
+	D2D1_SIZE_F levelSize  = m_SpriteSheet.GetSize();
 
-	D2D1_SIZE_F levelSize = m_SpriteSheet.GetSize();
-
-	float blockSize = windowSize.width / 3.f;
+	float blockSize = windowSize.width / 3.0f;
 
 	if (m_pPlayer->GetPoint().x + blockSize > levelSize.width - blockSize) {
 		Graphics::GetInstance()->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Translation(((levelSize.width - blockSize) * -1) + (blockSize * 2), 0));
@@ -62,24 +60,19 @@ void TestLevel::Render()
 	}
 
 	m_SpriteSheet.Draw();
-
 	m_pPlayer->Draw();
 
-	for (Enemy enemy : m_RenderEnemy)
-	{
+	for (Enemy enemy : m_RenderEnemy){
 		enemy.Draw();
 	}
-
 	// Draw UI component after 69 line.
 	Graphics::GetInstance()->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Identity());
-
 	m_playerHealthUI.Draw();
 }
 
 void TestLevel::Update(float dt)
 {
 	m_pPlayer->Update(*m_pInput, dt);
-	
 	m_pPlayer->UpdateCollision(&m_RenderEnemy);
 }
 
--- TestLevel.h
+++ TestLevel.h
@@ -10,27 +10,22 @@
 class TestLevel : public Level
 {
 private:
-	SpriteSheet m_SpriteSheet;
-
+	SpriteSheet		m_SpriteSheet;
+	DX_Input*		m_pInput;
+	PlayerHealthUI	m_playerHealthUI;
 	D2D1_RENDER_TARGET_PROPERTIES m_renderProperties;
-
-	DX_Input * m_pInput;
-
 	bool m_bIsInitialized;
 
 	//Render List
 	Player * m_pPlayer;
-	std::vector<Enemy> m_RenderEnemy;
+	std::vector<Enemy>  m_RenderEnemy;
 	std::vector<IActor> m_RenderEnvir;
-
-	// player Health gui.
-	PlayerHealthUI m_playerHealthUI;
 
 public:
 	TestLevel(DX_Input * pInput);
-	TestLevel(Player * player, DX_Input * pInput);
+	TestLevel(DX_Input * pInput, Player * pPlayer);
 	~TestLevel();
-	
+
 	virtual void Load()   override;
 	virtual void Unload() override;
 	virtual void Render() override;
